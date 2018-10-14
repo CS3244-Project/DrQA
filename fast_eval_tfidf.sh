d0afff7f8e6ebced15574ce47ac93abf5b4ed1cd
Luong Quoc Trung <lqtrung1998@gmail.com>
2018-10-14 16:31:20 +0800

    CheckPredict

---
+++ fast_eval_tfidf.sh
@@ -0,0 +1,2 @@
+python scripts/retriever/eval.py data/datasets/ln_dev.txt --model saved_tfidf/Combined-SQuAD-LNQA-tfidf-ngram\=2-hash\=16777216-tokenizer\=simple.npz  --doc-db saved_db/Combined-SQuAD-LNQA.db --n-docs 5
+
