2849e40f1769cd471a474d05c04b3a3ade657e04
Luong Quoc Trung <lqtrung1998@gmail.com>
2018-10-14 16:36:48 +0800

    CheckPredict

--- fast_build_db.sh
+++ fast_build_db.sh
@@ -1,3 +1,6 @@
 rm saved_db/*
-python scripts/retriever/build_db.py data/datasets/Combined-SQuAD-LNQA.json saved_db/Combined-SQuAD-LNQA.db --preprocess scripts/retriever/prep_lecture_note.py
+python scripts/retriever/build_db.py data/datasets/SQuAD-v1.1-train.json saved_db/SQuAD-v1.1-train.db --preprocess scripts/retriever/prep_lecture_note.py
+python scripts/retriever/build_db.py data/datasets/ln_train.json saved_db/ln_train.db --preprocess scripts/retriever/prep_lecture_note.py
+python scripts/retriever/build_db.py data/datasets/Combined-SQuAD-LNQA.json.json saved_db/Combined-SQuAD-LNQA.json.db --preprocess scripts/retriever/prep_lecture_note.py
+
 
