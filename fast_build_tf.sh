6a9877efcd0a878a0b819982d48e11000114a8b6
Luong Quoc Trung <lqtrung1998@gmail.com>
2018-10-14 16:36:48 +0800

    CheckPredict

--- fast_build_tf.sh
+++ fast_build_tf.sh
@@ -1,2 +1,4 @@
 rm saved_tfidf/*
-python scripts/retriever/build_tfidf.py saved_db/Combined-SQuAD-LNQA.db saved_tfidf/ 
+python scripts/retriever/build_tfidf.py saved_db/ln_train.db saved_tfidf/
+python scripts/retriever/build_tfidf.py saved_db/SQuAD-v1.1-train.db saved_tfidf/ 
+python scripts/retriever/build_tfidf.py saved_db/Combined-SQuAD-LNQA.db saved_tfidf/
