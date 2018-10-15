9090de9ccdf07c42cf3efb06bc0c39623e86da01
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2018-10-15 12:36:46 +0800

    Finalise Tf-idf scripts

--- fast_build_db.sh
+++ fast_build_db.sh
@@ -1,6 +1,4 @@
 rm saved_db/*
-python scripts/retriever/build_db.py data/datasets/SQuAD-v1.1-train.json saved_db/SQuAD-v1.1-train.db --preprocess scripts/retriever/prep_lecture_note.py
-python scripts/retriever/build_db.py data/datasets/ln_train.json saved_db/ln_train.db --preprocess scripts/retriever/prep_lecture_note.py
-python scripts/retriever/build_db.py data/datasets/Combined-SQuAD-LNQA.json.json saved_db/Combined-SQuAD-LNQA.json.db --preprocess scripts/retriever/prep_lecture_note.py
-
-
+python scripts/retriever/build_db.py data/datasets/ln_dev.json saved_db/ln_dev.db --preprocess scripts/retriever/prep_lecture_note.py
+python scripts/retriever/build_db.py data/datasets/ln.json saved_db/ln.db --preprocess scripts/retriever/prep_lecture_note.py
+python scripts/retriever/build_db.py data/datasets/SQuAD.json saved_db/squad.db --preprocess scripts/retriever/prep_lecture_note.py
--- fast_eval_tfidf.sh
+++ fast_eval_tfidf.sh
@@ -1,2 +1,6 @@
-python scripts/retriever/eval.py data/datasets/ln_dev.txt --model saved_tfidf/Combined-SQuAD-LNQA-tfidf-ngram\=2-hash\=16777216-tokenizer\=simple.npz  --doc-db saved_db/Combined-SQuAD-LNQA.db --n-docs 5
-
+python scripts/retriever/eval.py data/datasets/ln.txt --model saved_tfidf/ln-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz  --doc-db saved_db/ln.db --n-docs 5
+python scripts/retriever/eval.py data/datasets/ln.txt --model saved_tfidf/ln-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz  --doc-db saved_db/ln.db --n-docs 1
+python scripts/retriever/eval.py data/datasets/ln_dev.txt --model saved_tfidf/ln_dev-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz  --doc-db saved_db/ln_dev.db --n-docs 5
+python scripts/retriever/eval.py data/datasets/ln_dev.txt --model saved_tfidf/ln_dev-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz  --doc-db saved_db/ln_dev.db --n-docs 1
+python scripts/retriever/eval.py data/datasets/SQuAD.txt --model saved_tfidf/squad-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz  --doc-db saved_db/squad.db --n-docs 5
+python scripts/retriever/eval.py data/datasets/SQuAD.txt --model saved_tfidf/squad-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz  --doc-db saved_db/squad.db --n-docs 1
