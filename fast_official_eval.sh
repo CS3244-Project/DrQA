c9dab2e823142c3784d4806e3c33924d8188f5dc
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2018-10-16 21:11:56 +0800

    Updating fast scripts

--- fast_official_eval.sh
+++ fast_official_eval.sh
@@ -1,1 +1,4 @@
-python scripts/reader/official_eval.py data/datasets/ln_dev.json tmp/ln_no_init/ln_dev-single.preds
+python scripts/reader/official_eval.py data/datasets/ln_dev.json models/pre_trained_single/ln_dev-single.preds
+python scripts/reader/official_eval.py data/datasets/ln_dev.json models/pre_trained_multi/ln_dev-multitask.preds
+python scripts/reader/official_eval.py data/datasets/ln_dev.json models/csQA_single/ln_dev-csQA_single.preds
+python scripts/reader/official_eval.py data/datasets/ln_dev.json models/wsQA_single/ln_dev-wsQA_single.preds
