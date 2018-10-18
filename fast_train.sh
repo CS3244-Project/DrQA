942b1f101105f15b34d950e9485631be93771df0
Luong Quoc Trung <lqtrung1998@gmail.com>
2018-10-18 20:17:11 +0800

    Update Showing F1_Train, EM_Train

--- fast_train.sh
+++ fast_train.sh
@@ -1,3 +1,7 @@
+rm models/csQA_single/*
+#rm models/csQA_multitask/*
+rm models/wsQA_single/*
+#rm models/wsQA_multitask/*
 python scripts/reader/train.py --embedding-file glove.840B.300d.txt --tune-partial 1000 --model-dir models/csQA_single/ --model-name csQA_single --train-file ln_train-processed-corenlp.txt --dev-file ln_dev-processed-corenlp.txt --dev-json ln_dev.json
 # python scripts/reader/train.py --embedding-file glove.840B.300d.txt --tune-partial 1000 --model-dir models/csQA_multi/ --model csQA_multitask --train-file ln_train-processed-corenlp.txt --dev-file ln_dev-processed-corenlp.txt --dev-json ln_dev.json
 python scripts/reader/train.py --embedding-file glove.840B.300d.txt --tune-partial 1000 --model-dir models/wsQA_single/ --model-name wsQA_single --train-file ln_train-processed-corenlp.txt --dev-file ln_dev-processed-corenlp.txt --dev-json ln_dev.json --pretrained models/pre_trained_single/single.mdl
