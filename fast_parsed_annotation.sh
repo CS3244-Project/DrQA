f7b9c6ec75dd84509cc1911d100c6650b8ad0369
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2018-10-20 12:00:27 +0800

    Fix typos and scripts

--- fast_parsed_annotation.sh
+++ fast_parsed_annotation.sh
@@ -1,3 +1,3 @@
-python scripts/notes/annotation.py data/notes/mturk_cleaned.csv data/datasets/ln.json
-python scripts/notes/annotation.py data/notes/mturk_cleaned_train.csv data/datasets/ln_train.json
-python scripts/notes/annotation.py data/notes/mturk_cleaned_dev.csv data/datasets/ln_dev.json
+python scripts/notes/annotation.py data/notes/mturk_self.csv data/datasets/ln.json
+python scripts/notes/annotation.py data/notes/mturk_self_train.csv data/datasets/ln_train.json
+python scripts/notes/annotation.py data/notes/mturk_self_dev.csv data/datasets/ln_dev.json
