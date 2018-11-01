0f3ae4a876788d6619f857cc097939e038783ac8
nguyenvanhoang7398 <nguyenvanhoang7398@gmail.com>
2018-11-01 19:27:09 +0100

    Add sort dept

---
+++ scripts/sort_dept.py
@@ -0,0 +1,17 @@
+import constants
+import pickle
+import utils
+
+with open("ln.p") as f:
+	dataset = pickle.load(f)
+
+dataset_dict = {}
+
+for data in dataset:
+	title, annotated_p, question, answer, dept, chapter = data
+	if dept is not in dataset_dict:
+		dataset_dict[dept] = []
+	dataset_dict[dept].append(data)
+
+for i, k in enumerate(dataset_dict.keys()):
+	utils.write2csv(dataset_dict[k], "depts/dept_" + str(i)+ ".csv", constants.note_tsv_header)
