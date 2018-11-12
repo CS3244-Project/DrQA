d73395b621acd53805288e1206f3b9e35e46b791
Luong Quoc Trung <lqtrung1998@gmail.com>
2018-11-12 19:32:50 +0800

    Completing Interactive

--- scripts/notes/pdf2json.py
+++ scripts/notes/pdf2json.py
@@ -1,7 +1,8 @@
 import json
 import pdf_reader
 import utils
-
+import os
+import subprocess
 def pdf2json(file_paths, json_out, squash=True, titles=None):
 	data = {'data': [], 'version': '1.1'}
 
@@ -25,4 +26,13 @@ def pdf2json(file_paths, json_out, squash=True, titles=None):
 		json.dump(data, f)
 
 if __name__ == "__main__":
-	pdf2json(["data/notes/Photosynthesis.pdf", "data/notes/Plant Physiology.pdf"], "out.json")
+	#pdf2json(["data/notes/Photosynthesis.pdf", "data/notes/Plant Physiology.pdf"], "out.json")
+	pdfs = []
+	for pdf in os.listdir("step_demo/pdfs/"):
+		pdfs.append("step_demo/pdfs/"+pdf)
+	pdf2json(pdfs,"step_demo/combined_pdfs/combined.json")
+	subprocess.call(["bash","-c","rm step_demo/db/*"])
+	subprocess.call(["bash","-c","python scripts/retriever/build_db.py step_demo/combined_pdfs/combined.json step_demo/db/LN.db --preprocess scripts/retriever/prep_lecture_note.py"])
+	subprocess.call(["bash","-c","python scripts/retriever/build_tfidf.py step_demo/db/LN.db step_demo/retriever_model/"])
+	subprocess.call(["bash","-c","python scripts/pipeline/interactive.py --reader-model step_demo/reader_model/STeP_LNQA.mdl --retriever-model step_demo/retriever_model/LN-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --doc-db step_demo/db/LN.db"])
+
